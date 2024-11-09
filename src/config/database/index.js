d7719fccab963a05b0821628d6b02f4137e09aa9
vuthevy1209 <vuthevy12092004@gmail.com>
2024-11-09 12:47:52 +0700

    connect to MongoDB Atlas

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+
+const uri = "mongodb+srv://root:ABC123@ga03.dhlfb.mongodb.net/GA03?retryWrites=true&w=majority&appName=GA03";
+//const uri = "mongodb://localhost:27017/GA03";
+
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/GA03');
+        await mongoose.connect(uri);
         console.log('Connect successfully!!!');
     } catch (error) {
         console.error('Connect failure!!!', error);
